fd716f2b8577c46a58770c21295bf00d71189b5b
tez-dev <85144666+tez-dev@users.noreply.github.com>
2026-01-14 21:35:22 +0900

    chore : エラー表示をlog_error関数に変更

--- srcs/debug_rt_loader.c
+++ srcs/debug_rt_loader.c
@@ -6,7 +6,7 @@
 /*   By: htsutsum <htsutsum@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/14 17:25:42 by htsutsum          #+#    #+#             */
-/*   Updated: 2026/01/14 18:14:20 by htsutsum         ###   ########.fr       */
+/*   Updated: 2026/01/14 21:15:13 by htsutsum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,10 @@ int	main(int argc, char **argv)
 	printf("Source file: %s\n", argv[1]);
     if (rt_loader(&objs, argv[1]) != 0)
     {
-        printf("Error: Failed to load scene from %s\n", argv[1]);
+        printf("Error: Failed to load object from %s\n", argv[1]);
         free_objects(objs);
         return (1);
     }
-
-    // デバッグ表示
     printf("Source file: %s\n", argv[1]);
 	debug_print_objects(objs);
 
@@ -45,7 +43,6 @@ static void debug_print_objects(t_object *obj)
         printf("--- Object [%d] ---\n", i++);
         printf("Type: %d (0:SPHERE, 1:PLANE, 2:CYLINDER)\n", obj->type);
         printf("Point: (%f, %f, %f)\n", obj->point.x, obj->point.y, obj->point.z);
-        printf("Color: (%f, %f, %f)\n", obj->color.x, obj->color.y, obj->color.z);
 
         if (obj->type == SPHERE)
             printf("Sphere Radius: %f\n", obj->data.sp.radius);
@@ -56,6 +53,7 @@ static void debug_print_objects(t_object *obj)
             printf("Cylinder Normal: (%f, %f, %f)\n", obj->data.cy.normal.x, obj->data.cy.normal.y, obj->data.cy.normal.z);
             printf("Cylinder Radius: %f, Height: %f\n", obj->data.cy.radius, obj->data.cy.height);
         }
+        printf("Color: (%f, %f, %f)\n", obj->color.x, obj->color.y, obj->color.z);
         obj = obj->next;
         printf("\n");
     }
