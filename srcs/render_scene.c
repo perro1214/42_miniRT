3c242a2c4c0e678651fa588d70c87781833b0d7b
tez-dev <85144666+tez-dev@users.noreply.github.com>
2026-02-10 15:32:41 +0900

    feat : render_scenee.cにfind_closest_obj関数追加、raycast関数での環境工、ライティング、影の計算位置にコメントを追加

--- srcs/render_scene.c
+++ srcs/render_scene.c
@@ -6,14 +6,15 @@
 /*   By: htsutsum <htsutsum@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 19:50:29 by htsutsum          #+#    #+#             */
-/*   Updated: 2026/02/10 09:15:39 by htsutsum         ###   ########.fr       */
+/*   Updated: 2026/02/10 15:29:14 by htsutsum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
 
-t_vec3	raycast(t_scene *scene, t_ray ray);
+t_vec3		raycast(t_scene *scene, t_ray ray);
+t_object	*find_closest_obj(t_scene *scene, t_ray ray, double *out_t);
 
 // レンダリング
 void	render_scene(t_scene *scene)
@@ -40,21 +41,34 @@ void	render_scene(t_scene *scene)
 		y++;
 	}
 	mlx_put_image_to_window(scene->mlx->mlx, scene->mlx->win, scene->mlx->img, 0, 0);
-
 }
 
 // レイが光が当たるかどうか。
 t_vec3	raycast(t_scene *scene, t_ray ray)
+{
+	t_object	*closest_obj;
+	double		min_t;
+
+	closest_obj = find_closest_obj(scene, ray, &min_t);
+	if (closest_obj)
+	{
+		// この辺に、環境光、ライティング、影の計算を開始 ->計算したカラーをリターン
+		return (closest_obj->color); // 当たった物体の色
+	}
+	return (vec3_init(0.1, 0.1, 0.1)); // 背景色
+}
+
+// カメラから近いオブジェクトを判定
+t_object	*find_closest_obj(t_scene *scene, t_ray ray, double *out_t)
 {
 	t_object	*curr;
 	t_object	*closest_obj;
 	double		min_t;
 	double		t;
 
-	closest_obj = NULL;
-	min_t = INFINITY; // 初期値は無限
 	curr = scene->objs;
-	// オブジェクトがあればループ
+	closest_obj = NULL;
+	min_t = INFINITY;
 	while (curr)
 	{
 		t = -1.0;
@@ -72,7 +86,6 @@ t_vec3	raycast(t_scene *scene, t_ray ray)
 		}
 		curr = curr->next;
 	}
-	if (closest_obj)
-		return (closest_obj->color); // 当たった物体の色
-	return (vec3_init(0.1, 0.1, 0.1)); // 背景色
+	*out_t = min_t;
+	return (closest_obj);
 }
